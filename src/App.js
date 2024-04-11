6dcea5633779ff968b0d7d6aeef9f54524e7aa4f
Ani <146964321+AniKhanamiryann@users.noreply.github.com>
2024-04-11 22:22:09 +0400

    disappointed commit)

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
         <header className="App-header">
           <h1>Welcome to My React App</h1>
         </header>
-        <main>
+        <div>
           <PostsList/>
-        </main>
+        </div>
       </div>
   );
 }
